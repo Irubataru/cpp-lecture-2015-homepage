826694eb4287ac1ad1cb5d07ec79b2ccb70f32b3
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2015-08-19 22:54:34 +0200

    Some more project management files

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 html_path="th.physik.uni-frankfurt.de:public_html/"
 all_files=$(git ls-tree -r HEAD --name-only)
-send_files=$(echo $all_files | awk '{gsub("install.sh|.gitignore","");print}')
+send_files=$(echo $all_files | awk '{gsub("install.sh|.gitignore|content.md|.tern-project","");print}')
 
-scp $send_files $html_path
+#scp $send_files $html_path
+echo $send_files
