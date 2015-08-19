389b9c21a30ff76dfab287ae83d9488d193707c2
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2015-08-20 01:26:05 +0200

    Added the anit-injection function
    ref: http://www.html-form-guide.com/email-form/php-form-to-email.html

--- php/emailer.php
+++ php/emailer.php
@@ -49,7 +49,33 @@ function sanitize($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
+
+  if (IsInjected($data)) {
+    return "";
+  }
+
   return $data;
+}
+
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+                
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+     
+    if(preg_match($inject,$str)) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 ?>
