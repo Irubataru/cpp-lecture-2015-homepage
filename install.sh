5de97c7c949cab4e5c7aff1150d1ef82597dea3e
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2015-08-20 19:01:17 +0200

    Made a more awesome install script

--- install.sh
+++ install.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-html_path="th.physik.uni-frankfurt.de:public_html/"
-all_files=$(git ls-tree -r HEAD --name-only)
-send_files=$(echo $all_files | awk '{gsub("install.sh|.gitignore|content.md|.tern-project","");print}')
+# Some script constants
+exclude_files=("install.sh" "content.md")
+ssh_domain="th.physik.uni-frankfurt.de"
+website_folder="public_htm/"
 
-#scp $send_files $html_path
-echo $send_files
+# Test if I am currently on the master branch
+master_branch="master"
+current_branch="$(git rev-parse --abbrev-ref HEAD | sed -e 's/[[:space:]]//g')"
+
+if [ $current_branch != $master_branch ] ; then
+  echo "Not on $master_branch, are you sure you want to push website?"
+  select updaterepo in "Yes" "No"; do
+    case $updaterepo in
+      Yes ) break;;
+      No ) exit
+    esac
+  done
+fi
+
+# Put together the exclude flags to pass to the rsync function
+exclude_flag="--exclude=.git"
+
+for file in "${exclude_files[@]}" ; do
+  exclude_flag="$exclude_flag --exclude=$file"
+done
+
+# Create the ssh path
+ssh_path=$ssh_domain":"$website_folder
+
+# Finally send it all to the server
+rsync -PSauve ssh $exclude_flag * $ssh_path
